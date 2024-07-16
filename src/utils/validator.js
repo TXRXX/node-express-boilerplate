4c37eb32a85a3221e85c41dc6827f4a2780d32b8
TXRXX <toro2544@gmail.com>
2024-07-16 17:58:59 +0700

    Export utils inline

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,11 +1,9 @@
-const validateUsername = (username) => {
+export const validateUsername = (username) => {
     const re = /^[a-zA-Z0-9]{3,30}$/;
     return re.test(username);
 };
 
-const validatePassword = (password) => {
+export const validatePassword = (password) => {
     const re = /^[a-zA-Z0-9]{8,30}$/;
     return re.test(password);
 };
-
-module.exports = { validateUsername, validatePassword };
